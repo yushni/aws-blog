e4f6159acb1f453a0dadd9c90b4876ad767a3591
yurashni <yuriy.shnitsar@gmail.com>
2020-02-07 15:03:31 +0200

    move all functional to one do method for lambda

--- main.go
+++ main.go
@@ -1,137 +1,45 @@
 package main
 
 import (
-	"fmt"
-	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"log"
-	"net"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", info)
-	http.HandleFunc("/post/create", post)
-	http.HandleFunc("/post/show", show)
+	db, err := NewDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/", do(db))
 
 	port := ":8081"
 	log.Printf("start work on %s", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
 
-func info(writer http.ResponseWriter, _ *http.Request) {
-	ifaces, err := net.Interfaces()
-	if err != nil {
+func do(db *DB) func(http.ResponseWriter, *http.Request) {
+	var err error
+	respErr := func(writer http.ResponseWriter, request *http.Request) {
 		writer.Write([]byte(err.Error()))
 	}
 
-	for _, iface := range ifaces {
-		if iface.Flags&net.FlagUp == 0 {
-			continue // interface down
-		}
-		if iface.Flags&net.FlagLoopback != 0 {
-			continue // loopback interface
-		}
-
-		addrs, err := iface.Addrs()
-		if err != nil {
-			response(writer, err.Error())
-			return
-		}
-
-		for _, addr := range addrs {
-			var ip net.IP
-			switch v := addr.(type) {
-			case *net.IPNet:
-				ip = v.IP
-			case *net.IPAddr:
-				ip = v.IP
-			}
-			if ip == nil || ip.IsLoopback() {
-				continue
-			}
-			ip = ip.To4()
-			if ip == nil {
-				continue // not an ipv4 address
-			}
-
-			response(writer, "your ip is: "+ip.String())
-			return
-		}
+	if err = db.Create(); err != nil {
+		return respErr
 	}
 
-	response(writer, "are you connected to the network?")
-}
-
-func post(writer http.ResponseWriter, _ *http.Request) {
-	db, err := connect()
+	res, err := db.All()
 	if err != nil {
-		response(writer, err.Error())
-		return
+		return respErr
 	}
 
-	st, err := db.Prepare("INSERT INTO person (name) VALUES (md5(random()::text))")
+	ip, err := MyIP()
 	if err != nil {
-		response(writer, err.Error())
-		return
+		return respErr
 	}
 
-	_, err = st.Exec()
-	if err != nil {
-		response(writer, err.Error())
+	return func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("IP: " + ip + " \n" + res))
 	}
-
-	response(writer, "success")
-}
-
-func show(writer http.ResponseWriter, _ *http.Request) {
-	db, err := connect()
-	if err != nil {
-		response(writer, err.Error())
-		return
-	}
-
-	st, err := db.Prepare("Select * from person")
-	if err != nil {
-		response(writer, err.Error())
-		return
-	}
-
-	res, err := st.Query()
-	if err != nil {
-		response(writer, err.Error())
-		return
-	}
-
-	var data string
-	for res.Next() {
-		var Id int
-		var Name string
-
-		if err = res.Scan(&Id, &Name); err != nil {
-			response(writer, err.Error())
-			return
-		}
-
-		data += fmt.Sprintf("ID: %d, Name: %s \n", Id, Name)
-	}
-
-	response(writer, data)
-}
-
-func connect() (*sqlx.DB, error) {
-	host := "rds-test.cnhtbv3maxil.eu-central-1.rds.amazonaws.com"
-	port := "5432"
-	user := "postgres"
-	dbname := "postgres"
-	password := "1234567890"
-
-	dns := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-
-	return sqlx.Open("postgres", dns)
-}
-
-func response(writer http.ResponseWriter, resp string) {
-	writer.Write([]byte(resp))
 }
